f6b8ca9c99bf3d43c6df0e5c23b9e5b8342f0df2
PuQing <me@puqing.work>
2022-03-23 14:09:08 +0800

    add ScanScreen

--- components/Home.js
+++ components/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Grade from './Grade';
-// import Scan from './Scan';
+import ScanScreen from './ScanScreen';
 import LinearGradient from 'react-native-linear-gradient';
 import {
   StyleSheet,
@@ -8,9 +8,13 @@ import {
   Image,
   View,
   Text,
+  TouchableOpacity,
+  Linking,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
 
 export default class Home extends Component {
   constructor(props) {
@@ -36,11 +40,30 @@ export default class Home extends Component {
     );
   };
 
-  _Scan = ({navigation}) => {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
+
+  ScanScreen = ({navigation}) => {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Details Screen</Text>
-      </View>
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
     );
   };
 
@@ -50,7 +73,7 @@ export default class Home extends Component {
       <NavigationContainer independent={true}>
         <Stack.Navigator initialRouteName="Home">
           <Stack.Screen name="Home" component={this._Home} />
-          <Stack.Screen name="Scan" component={this._Scan} />
+          <Stack.Screen name="Scan" component={this.ScanScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -66,5 +89,22 @@ var styles = StyleSheet.create({
   image: {
     width: 100,
     height: 100,
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
   },
 });
---
+++ components/ScanScreen.js
@@ -0,0 +1,49 @@
+'use strict';
+
+import React, {Component} from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {RNCamera} from 'react-native-camera';
+
+class ScanScreen extends Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
+
+  render() {
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to{' '}
+            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+            your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+AppRegistry.registerComponent('default', () => ScanScreen);
